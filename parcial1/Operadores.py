37648660e03912efbde5c1ac8c0b176b40a0a306
AlinaSolis <169116206+AlinaSolis@users.noreply.github.com>
2024-05-23 19:51:22 -0600

    actualiza2

---
+++ parcial1/Operadores.py
@@ -0,0 +1,18 @@
+#Operaadores Aritmeticos
+#Arithmetic Operators
+#Operador Name Example
+print("Operadores:Aritmeticos")
+#print("+	Suma	x + y ")
+#Ejemplo
+x = 5
+y = 3
+print( "La suma es: ", x + y)
+
+# Resta : -	Subtraction	x - y
+num=0
+num1=0
+re=0
+print("Ingresa un numero:" , num,"Ingresa otro numero" , num, "El resultado es ",re )
+re=num = num1
+
+

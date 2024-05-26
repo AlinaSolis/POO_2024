c0194e5e68fbb2631e430a8202e34839642d9b62
AlinaSolis <169116206+AlinaSolis@users.noreply.github.com>
2024-05-25 19:24:20 -0600

    ac2

--- parcial1/6_Ejercicios_repaso_1al5/Exam.py
+++ parcial1/6_Ejercicios_repaso_1al5/Exam.py
@@ -1,36 +1,39 @@
     #k
-siono ="SI"
-siono.upper()
+#Para la funcion .upper, necesitamos una variable, es decir en este caso, usamos "siono=siono.upper"
+sueldo_mes=0
 contador = 0
 cap = 0
-
+acumulador=0
 while True:
- if siono == "SI":
-     nomta = input("Intrudisca el Nombre del trabajador: ")
-     hora = float(input("Intrudisca las Horas trabajadas: "))
-     dias = float(input("Intrudisca dias trabajadas: "))
-     suel = float(input("Intrudisca el sueldo por horas: "))
+
+   nomta = input("Intrudisca el Nombre del trabajador: ")
+   hora = float(input("Intrudisca las Horas trabajadas: "))
+   dias = float(input("Intrudisca dias trabajadas: "))
+   suel = float(input("Intrudisca el sueldo por horas: "))
 #LEa    
-     sueldo_diari = hora *  suel
-     sueldo_semana = sueldo_diari * dias
+   sueldo_diari = hora *  suel
+   sueldo_semana = sueldo_diari * dias
 #LEb
-     print(sueldo_semana)
+   print(sueldo_semana)
 
-     sueldo_mes = sueldo_semana * 4
-     if sueldo_mes >= 10000:
-        print("Obrero tipo A")
+   sueldo_mes=sueldo_semana * 4
+   print(sueldo_mes)
+   if sueldo_mes >= 10000:
+      print("Obrero tipo A")
 
-     elif sueldo_mes > 10000 or sueldo_mes < 15000:
-        print("Obrero tipo B")
-     else:
-        print("Sin categoria")
+   elif sueldo_mes > 10000 and sueldo_mes < 15000:
+      print("Obrero tipo B")
+   else:
+      print("Sin categoria")
 #LEc
-     cap=float(print("Impresion de todas las capturas: "))
-     
-     siono=str(input("¿Deseas otra captura?: (SI/NO)"))
-     siono=siono.upper()
-
-     break
+   contador=contador+1
+   acumulador=sueldo_mes+acumulador
+   siono=str(input("¿Deseas otra captura?: (SI/NO)"))
+   siono=siono.upper()
+   if siono=="NO":
+      break
   
+print(contador)
+print("Sueldo de los trabajadores: ", acumulador)
 
 

54778525cf9961ce3855845f770bfbf34c41b5bc
AlinaSolis <169116206+AlinaSolis@users.noreply.github.com>
2024-07-12 11:27:50 -0600

    ER Parcial3 CRUD

--- parcial3/CRUD_BD/2_crearBD.py
+++ parcial3/CRUD_BD/2_crearBD.py
@@ -1,59 +1,16 @@
 import mysql.connector
 
-try:
-    #Crear la conexion con la BD
-    conexion=mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
+#conectarse con la base de datos de mysql
+conexion=mysql.connector.connect(
+    host="local host",
+    user="root",
+    password="",
+    database="bd_python"
+)
 
-    )
-#verificar la conexion
-    if conexion.is_connected():
-        print("Se creo la conexion exitosa")
-    else:
-        print("No fue posible conectarse")
-
-#crear otro objeto para ejecutar las conecciones 
-    micursor=conexion.cursor()
-
-#crear la bd desde python
-    sql="create database bd_python"
-    micursor.execute(sql)
-
-#verificar que se creo la BD 
-    if micursor:
-        print("Se creo la base de datos exitosamente")
-
-#mostrar las bases de datos que existen en my servidor en mi MYSQL
-    micursor.execute("show databases")
-
-    for x in micursor:
-        print(x)
-
-except:
-    print("Ocurrio un problema con el servidor... por favor intentalo mas tarde...")
-
+#verificar la conexion con la  BD
+if conexion.is_connected():
+    print("Se creo la conexion con la base de datos")
 else:
-    #verificar la conexion
-    if conexion.is_connected():
-        print("Se creo la conexion exitosa")
-    else:
-        print("No fue posible conectarse")
-
-#crear otro objeto para ejecutar las conecciones 
-    micursor=conexion.cursor()
-
-#crear la bd desde python
-    sql="create database bd_python"
-    micursor.execute(sql)
-
-#verificar que se creo la BD 
-    if micursor:
-        print("Se creo la base de datos exitosamente")
-
-#mostrar las bases de datos que existen en my servidor en mi MYSQL
-    micursor.execute("show databases")
-
-    for x in micursor:
-        print(x)
+    print("No fue posible conectar la base de datos .. verifique..")
+    #
--- parcial3/CRUD_BD/5_SeleccionarRegistro.py
+++
@@ -1,5 +0,0 @@
-if conexion.is_connected():
-    print("Se creo la conexion con la base de datos")
-else:
-    print("No fue posible conectar la base de datos ... verifique...")
- 
